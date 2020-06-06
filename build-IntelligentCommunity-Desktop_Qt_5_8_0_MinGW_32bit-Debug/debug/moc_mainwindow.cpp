95d2e5ae8638387494505e275da8666d23c42356
yang1584375824 <35062993+yang1584375824@users.noreply.github.com>
2020-06-06 19:44:38 +0800

    V0.0.5

--- build-IntelligentCommunity-Desktop_Qt_5_8_0_MinGW_32bit-Debug/debug/moc_mainwindow.cpp
+++ build-IntelligentCommunity-Desktop_Qt_5_8_0_MinGW_32bit-Debug/debug/moc_mainwindow.cpp
@@ -21,8 +21,8 @@ QT_BEGIN_MOC_NAMESPACE
 QT_WARNING_PUSH
 QT_WARNING_DISABLE_DEPRECATED
 struct qt_meta_stringdata_MainWindow_t {
-    QByteArrayData data[8];
-    char stringdata0[145];
+    QByteArrayData data[6];
+    char stringdata0[95];
 };
 #define QT_MOC_LITERAL(idx, ofs, len) \
     Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
@@ -36,16 +36,12 @@ QT_MOC_LITERAL(1, 11, 21), // "on_pushButton_clicked"
 QT_MOC_LITERAL(2, 33, 0), // ""
 QT_MOC_LITERAL(3, 34, 7), // "checked"
 QT_MOC_LITERAL(4, 42, 28), // "on_commandLinkButton_clicked"
-QT_MOC_LITERAL(5, 71, 23), // "on_pushButton_2_clicked"
-QT_MOC_LITERAL(6, 95, 24), // "on_radioButton_3_clicked"
-QT_MOC_LITERAL(7, 120, 24) // "on_radioButton_2_clicked"
+QT_MOC_LITERAL(5, 71, 23) // "on_pushButton_2_clicked"
 
     },
     "MainWindow\0on_pushButton_clicked\0\0"
     "checked\0on_commandLinkButton_clicked\0"
-    "on_pushButton_2_clicked\0"
-    "on_radioButton_3_clicked\0"
-    "on_radioButton_2_clicked"
+    "on_pushButton_2_clicked"
 };
 #undef QT_MOC_LITERAL
 
@@ -55,7 +51,7 @@ static const uint qt_meta_data_MainWindow[] = {
        7,       // revision
        0,       // classname
        0,    0, // classinfo
-       5,   14, // methods
+       3,   14, // methods
        0,    0, // properties
        0,    0, // enums/sets
        0,    0, // constructors
@@ -63,18 +59,14 @@ static const uint qt_meta_data_MainWindow[] = {
        0,       // signalCount
 
  // slots: name, argc, parameters, tag, flags
-       1,    1,   39,    2, 0x08 /* Private */,
-       4,    1,   42,    2, 0x08 /* Private */,
-       5,    1,   45,    2, 0x08 /* Private */,
-       6,    0,   48,    2, 0x08 /* Private */,
-       7,    0,   49,    2, 0x08 /* Private */,
+       1,    1,   29,    2, 0x08 /* Private */,
+       4,    1,   32,    2, 0x08 /* Private */,
+       5,    1,   35,    2, 0x08 /* Private */,
 
  // slots: parameters
     QMetaType::Void, QMetaType::Bool,    3,
     QMetaType::Void, QMetaType::Bool,    3,
     QMetaType::Void, QMetaType::Bool,    3,
-    QMetaType::Void,
-    QMetaType::Void,
 
        0        // eod
 };
@@ -88,8 +80,6 @@ void MainWindow::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, 
         case 0: _t->on_pushButton_clicked((*reinterpret_cast< bool(*)>(_a[1]))); break;
         case 1: _t->on_commandLinkButton_clicked((*reinterpret_cast< bool(*)>(_a[1]))); break;
         case 2: _t->on_pushButton_2_clicked((*reinterpret_cast< bool(*)>(_a[1]))); break;
-        case 3: _t->on_radioButton_3_clicked(); break;
-        case 4: _t->on_radioButton_2_clicked(); break;
         default: ;
         }
     }
@@ -120,13 +110,13 @@ int MainWindow::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
     if (_id < 0)
         return _id;
     if (_c == QMetaObject::InvokeMetaMethod) {
-        if (_id < 5)
+        if (_id < 3)
             qt_static_metacall(this, _c, _id, _a);
-        _id -= 5;
+        _id -= 3;
     } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
-        if (_id < 5)
+        if (_id < 3)
             *reinterpret_cast<int*>(_a[0]) = -1;
-        _id -= 5;
+        _id -= 3;
     }
     return _id;
 }
